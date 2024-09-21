f816cb9c105af7ed89e62a218ddd27e4d690ea9a
siddhartha0 <siddhartha.sunuwar@gmail.com>
2024-09-21 12:49:29 +0545

    init

---
+++ front-end/src/constant/paths/profile-sidebar.ts
@@ -0,0 +1,20 @@
+export const ProfileSideBar = [
+  {
+    id: 1,
+    path: "/profile",
+    title: "Manage Profile",
+    icon: "material-symbols-light:settings-outline",
+  },
+  {
+    id: 2,
+    path: "/manage-bookings",
+    title: "Manage Bookings",
+    icon: "material-symbols-light:bookmarks-outline",
+  },
+  {
+    id: 3,
+    path: "/current-users",
+    title: "Current Users",
+    icon: "mdi:user-outline",
+  },
+];
