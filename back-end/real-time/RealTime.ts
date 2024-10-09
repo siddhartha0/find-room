acd1b4837f0f88ae4d68135b3d60cd67d835690d
siddhartha0 <siddhartha.sunuwar@gmail.com>
2024-10-09 08:12:46 +0545

    socket changes

--- back-end/real-time/RealTime.ts
+++ back-end/real-time/RealTime.ts
@@ -18,8 +18,13 @@ export const io = new Server(serverInstance, {
   },
 });
 
+const users: { [key: string]: string } = {};
+
 io.on("connection", (socket) => {
   console.log("Connect to Socket server", socket.id);
+  const userId = socket.handshake.query.userId as string;
+
+  if (userId) users[userId] = socket.id;
 
   socket.on("disconnect", () => {
     console.log("Disconnected from socket server", socket.id);
